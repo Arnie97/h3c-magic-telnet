baa788e782c31765845f46b772f48bf32fe27df0
Arnie97 <arnie97@gmail.com>
2026-02-27 11:49:53 +0800

    Retry on errors

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use netroute::RouteFamily::Ipv4;
 use serde::Deserialize;
 use std::io;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct BasicInfo {
     brand: String,
     model: String,
@@ -10,14 +10,21 @@ struct BasicInfo {
     mac: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 struct Resp<T> {
     code: i32,
     message: String,
     data: T,
 }
 
+#[derive(Debug, Deserialize)]
+struct RespItem<T> {
+    id: i32,
+    result: Resp<T>,
+}
+
 #[derive(Debug)]
+#[allow(dead_code)]
 enum ApiError {
     Network(String),
     Status(i32, String),
@@ -26,7 +33,7 @@ enum ApiError {
 
 fn call_api<T, E>(ip: &str, path: &str) -> Result<T, E>
 where
-    T: for<'de> Deserialize<'de>,
+    T: for<'de> Deserialize<'de> + Clone,
     E: From<ApiError>,
 {
     let url = format!("http://{}{}", ip, path);
@@ -36,14 +43,25 @@ where
         .send()
         .map_err(|e| ApiError::Network(e.to_string()))?;
     if response.status_code != 200 {
-        return Err(ApiError::Status(response.status_code, response.reason_phrase).into());
+        return Err(ApiError::Status(
+            response.status_code,
+            response.reason_phrase,
+        )
+        .into());
     }
     let contents = response
         .as_str()
         .map_err(|e| ApiError::Parse(e.to_string()))?;
     eprintln!("{}", contents);
-    let resp: Resp<T> =
-        serde_json::from_str(contents).map_err(|e| ApiError::Parse(e.to_string()))?;
+    let resp = match serde_json::from_str::<Vec<RespItem<T>>>(contents) {
+        Ok(resp_vec) => resp_vec
+            .get(0)
+            .ok_or(ApiError::Parse("Empty JSON array".to_owned()))?
+            .result
+            .clone(),
+        Err(_) => serde_json::from_str::<Resp<T>>(contents)
+            .map_err(|e| ApiError::Parse(e.to_string()))?,
+    };
     if resp.code != 0 {
         return Err(ApiError::Status(resp.code, resp.message).into());
     }
@@ -70,7 +88,7 @@ fn enable_telnet(
     call_api(host, &format!("/api/debug?status=enable{}", md5_hex))
 }
 
-fn get_gateways() -> Vec<String> {
+fn list_possible_gateways() -> Vec<String> {
     let mut hosts = Vec::new();
 
     // Try to detect gateway from default routes
@@ -98,40 +116,47 @@ fn get_gateways() -> Vec<String> {
     hosts
 }
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    // Find gateways
-    let hosts: Vec<String> = if args.len() > 1 {
-        args[1..].to_vec()
-    } else {
-        get_gateways()
-    };
-
-    // Try gateways in turn, and get hardware info
-    let mut found: Option<(String, BasicInfo)> = None;
+fn get_available_gateway(hosts: Vec<String>) -> Option<(String, BasicInfo)> {
     for host in &hosts {
         match get_basic_info(host) {
             Ok(info) => {
-                found = Some((host.clone(), info));
-                break;
+                return Some((host.clone(), info));
             }
             Err(e) => eprintln!("{:?}", e),
         }
     }
+    None
+}
 
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    try_gateways(if args.len() > 1 {
+        args[1..].to_vec()
+    } else {
+        list_possible_gateways()
+    });
+}
+
+fn try_gateways(gateways: Vec<String>) {
     // Read password from stdin
-    if let Some((host, info)) = found {
-        enable_telnet(&host, &info, &prompt("Password: ")).ok();
+    if let Some((host, info)) = get_available_gateway(gateways) {
+        while let Err(e) = enable_telnet(
+            &host,
+            &info,
+            &prompt(&format!("Password for {}:", host)),
+        ) {
+            eprintln!("{:?}", e);
+        }
     } else {
         eprintln!("\n{:?}", ApiError::Network("No gateway responded".into()));
+        return try_gateways(vec![prompt("Specify the IP manually:")]);
     }
 
     prompt("\nPress ENTER to exit...");
 }
 
 fn prompt(msg: &str) -> String {
-    print!("{}", msg);
+    print!("{} ", msg);
     io::Write::flush(&mut io::stdout()).ok();
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).ok();
